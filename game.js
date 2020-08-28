e4d8e1c24717c1d2832de79721382863133b9285
xboxps4 <67192301+xboxps4@users.noreply.github.com>
2020-08-28 20:13:16 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,75 @@
+function start()
+{
+  background(bg1);
+  image(title,1000,15);
+  textSize(30);
+  fill("red");
+  text("Shoot the enemies and get to finish line",850,300);
+  startButton.position(1100,500);
+  startButton.mousePressed(switching);
+  startButton.show();
+}
+
+function switching()
+{
+  gameState = "play"
+}
+
+function play()
+{
+  background("white");
+  if(keyDown(UP_ARROW))
+  {
+    mainPlayer.y = mainPlayer.y -30;
+  }
+
+  if(keyDown(DOWN_ARROW))
+  {
+     mainPlayer.y = mainPlayer.y + 30;
+  }
+  
+  startButton.hide();
+  endless.velocityX = -5;
+  enemies();
+  if(endless.x < 0)
+  {
+    endless.x = displayWidth/2;
+  }
+
+  if(keyDown("space"))
+  {
+    attack();
+  }
+
+  if(weaponGroup.isTouching(enemyGroup))
+  {
+    enemyGroup.destroyEach();
+    enemydead.play();
+    count = count + 2;
+  }
+
+
+  drawSprites();
+}
+
+function end()
+{
+  bulletGroup.setVelocityXEach(0);
+  enemyGroup.setVelocityXEach(0);
+  endless.velocityX = 0;
+  if(keyDown("space"))
+  {
+    gameState = "start";
+  }
+}
+
+function attack()
+{
+  var weapon = createSprite(20,40,20,20);
+  weapon.x = mainPlayer.x + 10;
+  weapon.y = mainPlayer.y;
+  weapon.velocityX = 5;
+  weapon.addImage(playerBulletImg);
+  weaponGroup.add(weapon);
+  weapon.scale = 0.2
+}
